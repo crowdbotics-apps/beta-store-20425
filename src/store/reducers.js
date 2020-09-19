c2764c487fe051d158254ed32223931111a794ff
Crowdbotics <team@crowdbotics.com>
2020-09-19 01:15:44 +0000

    Update store

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -3,5 +3,11 @@ import * as types from "./constants"
 const initialState = {}
 
 export default function apiReducer(state = initialState, action) {
-    return state
+  switch (action.type) {
+    case types.DOGAPI_GET_BREEDS_IMAGE_RANDOM_READ:
+    case types.DOGAPI_GET_BREEDS_IMAGE_RANDOM_READ_SUCCEEDED:
+    case types.DOGAPI_GET_BREEDS_IMAGE_RANDOM_READ_FAILED:
+    default:
+      return state
+  }
 }
